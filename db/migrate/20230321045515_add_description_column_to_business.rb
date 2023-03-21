885e57fd8734e0276d2ffc95ebb4938c080679de
slwenyu <123151170+slwenyu@users.noreply.github.com>
2023-03-21 15:51:42 +0800

    Revert "removed default from all models"

--- db/migrate/20230321045515_add_description_column_to_business.rb
+++ db/migrate/20230321045515_add_description_column_to_business.rb
@@ -1,5 +1,5 @@
 class AddDescriptionColumnToBusiness < ActiveRecord::Migration[7.0]
   def change
-    add_column :businesses, :description, :string, null: false
+    add_column :businesses, :description, :string, null: false, default: ""
   end
 end
