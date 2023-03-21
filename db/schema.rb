eeee546f442abb22f3cb8e4fbb191ff1c8bc37d6
Bali Tok <bali.tok@hotmail.com>
2023-03-21 11:15:54 +0800

    added description and category column to collection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_023915) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_031352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_023915) do
     t.bigint "business_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", default: "", null: false
+    t.string "category", default: "", null: false
     t.index ["business_id"], name: "index_collections_on_business_id"
   end
 
