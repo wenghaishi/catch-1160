db976f8464d4cb439fe03f21aaf43eb8d3c71409
shaun lee <sleewenyu@gmail.com>
2023-03-21 17:47:56 +0800

    Add banner attachment to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tokens, dependent: :destroy
   has_one_attached :photo
+  has_one_attached :banner
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
