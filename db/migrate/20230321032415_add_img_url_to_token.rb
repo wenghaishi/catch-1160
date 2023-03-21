e824564b26a3d41021392aaa1dc9d0c332649d5d
Bali Tok <bali.tok@hotmail.com>
2023-03-21 11:25:04 +0800

    added img url column to token

---
+++ db/migrate/20230321032415_add_img_url_to_token.rb
@@ -0,0 +1,5 @@
+class AddImgUrlToToken < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tokens, :img_url, :string, null: false, default: ""
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_031352) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_032415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_031352) do
     t.bigint "collection_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url", default: "", null: false
     t.index ["collection_id"], name: "index_tokens_on_collection_id"
   end
 
