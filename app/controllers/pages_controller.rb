6d23e72b166cf058f978bfa1a9a22edceca2a36a
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-24 10:22:16 +0800

    change branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
 
   def home
     return unless current_user
-
     if current_user.account_type == "business"
       redirect_to biz_business_path(current_user.business.id)
     elsif current_user.account_type == "user"
