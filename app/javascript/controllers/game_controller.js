1721fe263ddbe87de7150908b6595c80f03601ea
Bali Tok <bali.tok@hotmail.com>
2023-03-29 15:51:41 +0800

    made nft whack a mole game

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -6,59 +6,66 @@ export default class extends Controller {
 
   connect() {
     console.log("game controller connected yo");
-    console.log("yo");
-    console.log(this.holeTargets);
-    console.log(this.moleTargets);
-    console.log("wassup")
-    console.log(this.scoreTarget);
-    console.log(this.buttonTarget);
   }
+
 
   startGame() {
-    console.log("hello")
+    console.log("Game started");
+    let holes = this.holeTargets;
+    let moles = this.moleTargets;
+    let lastHole;
+    let timeUp = false;
+    let score = 0;
+
+    function randomTime(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+    }
+
+    function randomHole(holes) {
+      const idx = Math.floor(Math.random() * holes.length);
+      const hole = holes[idx];
+      return hole;
+    }
+
+    function peep() {
+      const time = randomTime(200, 1000);
+      const hole = randomHole(holes);
+      hole.classList.add('up');
+      setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+      }, time);
+    }
+
+
+
+    this.scoreTarget.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => {
+      console.log("Delayed for 1 second.");
+    }, "1000");
+    setTimeout(() => {
+      timeUp = true
+      score = this.scoreTarget.textContent;
+      if (score >= 2) {
+        alert("You won!");
+      } else {
+        alert("You lost!");
+      }
+    }, 10000)
+
   }
 
-}
-let lastHole;
-let timeUp = false;
-let score = 0;
-
-function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-function randomHole(holeTargets) {
-  const idx = Math.floor(Math.random() * holes.length);
-  const hole = holes[idx];
-  if (hole === lastHole) {
-    console.log('Ah nah thats the same one bud');
-    return randomHole(holes);
+  bonk(e) {
+    console.log("bonking")
+    console.log("trusted", e.isTrusted)
+    if(!e.isTrusted) return; // cheater!
+    console.log("scoreTargetBonked", this.scoreTarget)
+    let score = parseInt(this.scoreTarget.textContent);
+    score++;
+    this.element.parentNode.classList.remove('up');
+    this.scoreTarget.textContent = score;
   }
-  lastHole = hole;
-  return hole;
-}
-
-function peep() {
-  const time = randomTime(200, 1000);
-  const hole = randomHole(holes);
-  hole.classList.add('up');
-  setTimeout(() => {
-    hole.classList.remove('up');
-    if (!timeUp) peep();
-  }, time);
-}
-
-function startGame() {
-  scoreBoard.textContent = 0;
-  timeUp = false;
-  score = 0;
-  peep();
-  setTimeout(() => timeUp = true, 10000)
-}
-
-function bonk(e) {
-  if(!e.isTrusted) return; // cheater!
-  score++;
-  this.parentNode.classList.remove('up');
-  scoreBoard.textContent = score;
 }
